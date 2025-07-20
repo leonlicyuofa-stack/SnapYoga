08f2c7e1a380a5fac9fb398e7de9ec0b96d36911
Georgina Poon <georgina.pnyc@gmail.com>
2025-07-20 08:40:59 +0000

    use font: https://fonts.google.com/specimen/Lavishly+Yours?preview.text=

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -117,9 +117,9 @@ export default function WelcomePageAsRoot() {
         </header>
 
         <main className="relative z-10 flex flex-col items-center justify-center flex-grow text-center px-4 animate-in fade-in-0 slide-in-from-top-10 duration-1000 delay-200 pb-32">
-            <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight text-splash-foreground/90 mb-2 font-serif">{t('snapYogaTitle')}</h2>
-            <h1 className="text-2xl sm:text-3xl font-extrabold tracking-tight text-splash-foreground font-serif" dangerouslySetInnerHTML={{ __html: t('landingTitle').replace('Flow', '<b>Flow</b>').replace('Anytime', '<b>Anytime</b>') }}></h1>
-            <p className="mt-4 text-lg text-splash-foreground/80 max-w-md sm:text-xl">
+            <h2 className="text-2xl sm:text-3xl font-extrabold tracking-tight text-splash-foreground/90 mb-2">{t('snapYogaTitle')}</h2>
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight text-splash-foreground" dangerouslySetInnerHTML={{ __html: t('landingTitle').replace('Flow', '<b>Flow</b>').replace('Anytime', '<b>Anytime</b>') }}></h1>
+            <p className="mt-4 text-2xl text-splash-foreground/80 max-w-md sm:text-3xl font-serif">
             {t('landingSubtitle')}
             </p>
             <Button
