1d39707d818e1e3554bfc6f6bc9c56a62d57da48
Georgina Poon <georgina.pnyc@gmail.com>
2025-07-20 15:37:27 +0000

    the cats are still not overlapping

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -113,8 +113,8 @@ export default function HomePage() {
                 <path d="M 0,0 L 100,0 C 50,50 100,50 100,100 L 0,100 Z" fill="hsl(var(--splash-cat-pink))" />
                 <path d="M 0,100 C 50,50 0,50 0,0" fill="hsl(var(--splash-background))" />
 
-                {/* Light Blue Cat with Smiley Face and Blush */}
-                <g transform="translate(12, 12) scale(1.1)">
+                {/* Light Blue Cat with Bean Eyes and Blush */}
+                <g transform="translate(15, 30) scale(1.1)">
                     <path d="M 5,65 C 15,40 40,30 65,55 C 80,70 70,95 45,98 C 20,101 0,80 5,65 Z" fill="hsl(var(--splash-cat-blue-light))" />
                     {/* Airbrushed Blush */}
                     <ellipse cx="38" cy="78" rx="10" ry="7" fill="url(#blushGradient)" />
@@ -129,12 +129,12 @@ export default function HomePage() {
                 </g>
                 
                 {/* Pastel Green Cat */}
-                 <g transform="translate(15, 8) scale(0.9)">
+                 <g transform="translate(18, 20) scale(0.9)">
                     <path d="M 5,65 C 15,40 40,30 65,55 C 80,70 70,95 45,98 C 20,101 0,80 5,65 Z" fill="hsl(var(--splash-cat-green))" />
                 </g>
 
                 {/* Pastel Grey Cat */}
-                <g transform="translate(18, 4) scale(0.7)">
+                <g transform="translate(21, 10) scale(0.7)">
                     <path d="M 5,65 C 15,40 40,30 65,55 C 80,70 70,95 45,98 C 20,101 0,80 5,65 Z" fill="hsl(var(--splash-cat-grey))" />
                 </g>
             </svg>
