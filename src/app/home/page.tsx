0e421440c572b7f944cc78ec322110f74b369b30
Georgina Poon <georgina.pnyc@gmail.com>
2025-07-20 15:10:53 +0000

    overlap grey cat with green cat

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -127,12 +127,12 @@ export default function WelcomePageAsRoot() {
                 </g>
                 
                 {/* Pastel Green Cat */}
-                 <g transform="translate(10, 0) scale(0.7)">
+                 <g transform="translate(-5, -20) scale(0.7)">
                     <path d="M 5,65 C 15,40 40,30 65,55 C 80,70 70,95 45,98 C 20,101 0,80 5,65 Z" fill="hsl(var(--splash-cat-green))" />
                 </g>
 
                 {/* Pastel Grey Cat */}
-                <g transform="translate(26, -35) scale(0.5)">
+                <g transform="translate(0, -35) scale(0.5)">
                     <path d="M 5,65 C 15,40 40,30 65,55 C 80,70 70,95 45,98 C 20,101 0,80 5,65 Z" fill="hsl(var(--splash-cat-grey))" />
                 </g>
             </svg>
