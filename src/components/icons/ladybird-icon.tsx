ad1bca007c7ef508e7840cc4e4fd1523083e767c
Georgina Poon <georgina.pnyc@gmail.com>
2025-07-20 07:35:39 +0000

    use this color tone for the app

--- src/components/icons/ladybird-icon.tsx
+++ src/components/icons/ladybird-icon.tsx
@@ -6,23 +6,25 @@ export function LadybirdIcon(props: SVGProps<SVGSVGElement>) {
     <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" {...props}>
         <g transform="rotate(-15 50 50)">
             {/* Body */}
-            <circle cx="50" cy="50" r="35" fill="#D22B2B"/>
-            <circle cx="50" cy="50" r="35" stroke="black" strokeWidth="2"/>
+            <circle cx="50" cy="50" r="35" fill="hsl(var(--primary))"/>
+            <circle cx="50" cy="50" r="35" stroke="hsl(var(--primary-foreground))" strokeWidth="2"/>
             
             {/* Head */}
-            <circle cx="50" cy="20" r="15" fill="black" />
+            <circle cx="50" cy="20" r="15" fill="hsl(var(--primary-foreground))" />
             
             {/* Face */}
             <circle cx="45" cy="18" r="2" fill="white" />
             <circle cx="55" cy="18" r="2" fill="white" />
             
             {/* Spots */}
-            <circle cx="38" cy="40" r="5" fill="black" />
-            <circle cx="62" cy="40" r="5" fill="black" />
-            <circle cx="38" cy="65" r="5" fill="black" />
-            <circle cx="62" cy="65" r="5" fill="black" />
-            <circle cx="50" cy="52" r="5" fill="black" />
+            <circle cx="38" cy="40" r="5" fill="hsl(var(--primary-foreground))" />
+            <circle cx="62" cy="40" r="5" fill="hsl(var(--primary-foreground))" />
+            <circle cx="38" cy="65" r="5" fill="hsl(var(--primary-foreground))" />
+            <circle cx="62" cy="65" r="5" fill="hsl(var(--primary-foreground))" />
+            <circle cx="50" cy="52" r="5" fill="hsl(var(--primary-foreground))" />
         </g>
     </svg>
   );
 }
+
+    
