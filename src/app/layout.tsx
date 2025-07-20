44d0bbd330ab27f0f80489ef64c2696b1db30166
Georgina Poon <georgina.pnyc@gmail.com>
2025-07-20 16:37:20 +0000

    use this font for "SnapYoga"
    <link rel="preconnect" href="https://fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type {Metadata} from 'next';
-import { Inter, Shadows_Into_Light as ShadowsIntoLight } from 'next/font/google';
+import { Inter, Shadows_Into_Light } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/contexts/AuthContext'; 
@@ -11,7 +11,7 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-const shadowsIntoLight = ShadowsIntoLight({
+const shadowsIntoLight = Shadows_Into_Light({
   variable: '--font-shadows-into-light',
   subsets: ['latin'],
   weight: '400',
