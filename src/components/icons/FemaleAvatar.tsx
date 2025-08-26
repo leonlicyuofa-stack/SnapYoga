a3ddbf501728c2ffa6559f46dde2c10ead0458f0
Georgina Poon <georgina.pnyc@gmail.com>
2025-08-26 14:06:36 +0000

    soften the edges of the triangles of the femaleavatar and maleavataar

--- src/components/icons/FemaleAvatar.tsx
+++ src/components/icons/FemaleAvatar.tsx
@@ -6,7 +6,7 @@ export function FemaleAvatar(props: SVGProps<SVGSVGElement>) {
     <svg width="64" height="64" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <g fill="#F472B6">
             <circle cx="50" cy="25" r="20" />
-            <path d="M15 50 L50 100 L85 50 Z" />
+            <path d="M15 50 L40 100 Q50 110 60 100 L85 50 Z" />
         </g>
     </svg>
   );
--- src/components/icons/MaleAvatar.tsx
+++ src/components/icons/MaleAvatar.tsx
@@ -6,7 +6,7 @@ export function MaleAvatar(props: SVGProps<SVGSVGElement>) {
     <svg width="64" height="64" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g fill="#2DD4BF">
         <circle cx="50" cy="25" r="20" />
-        <path d="M15 100 L50 50 L85 100 Z" />
+        <path d="M15 100 L40 50 Q50 40 60 50 L85 100 Z" />
       </g>
     </svg>
   );
