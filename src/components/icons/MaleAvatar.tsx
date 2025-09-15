85b69d599f48cf2b8bd871fb3e0a25394a8e8737
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-15 15:24:15 +0000

    make the two dots smaller and slightly apart (_for element rect_)

--- src/components/icons/MaleAvatar.tsx
+++ src/components/icons/MaleAvatar.tsx
@@ -7,8 +7,8 @@ export function MaleAvatar(props: SVGProps<SVGSVGElement>) {
       <g>
         <rect x="15" y="15" width="70" height="70" rx="15" fill="#608398"/>
         {/* Eyes */}
-        <circle cx="42" cy="50" r="5" fill="#2C3E50" />
-        <circle cx="58" cy="50" r="5" fill="#2C3E50" />
+        <circle cx="40" cy="50" r="3" fill="#2C3E50" />
+        <circle cx="60" cy="50" r="3" fill="#2C3E50" />
       </g>
     </svg>
   );
