20f4fd89f2bd9d1ebeca0860335bbf60340eddf4
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-15 15:18:52 +0000

    replace the male avatar by this image

--- src/components/icons/MaleAvatar.tsx
+++ src/components/icons/MaleAvatar.tsx
@@ -3,19 +3,16 @@ import type { SVGProps } from 'react';
 
 export function MaleAvatar(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg width="80" height="80" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <g>
-        <path 
-          d="M 85,75 C 95,60 90,30 70,25 C 50,20 30,30 20,50 C 10,70 25,85 45,85 C 65,85 80,85 85,75 Z"
-          fill="#A7C7E7"
-          stroke="hsl(var(--foreground))"
-          strokeWidth="1.5"
-        />
-        {/* Smiley Face */}
-        <circle cx="68" cy="48" r="1.5" fill="hsl(var(--foreground))" />
-        <circle cx="78" cy="48" r="1.5" fill="hsl(var(--foreground))" />
-        <path d="M 70 56 Q 74 60 78 56" stroke="hsl(var(--foreground))" strokeWidth="1.5" fill="none" strokeLinecap="round" />
-      </g>
+    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path 
+        d="M 85,50 C 95,75 75,95 50,95 C 25,95 5,75 15,50 C 25,25 40,15 50,20 C 60,25 75,25 85,50 Z"
+        fill="#FFC700"
+      />
+      {/* Eyes */}
+      <circle cx="45" cy="50" r="4" fill="#2C3E50" />
+      <circle cx="65" cy="50" r="4" fill="#2C3E50" />
+      {/* Smile */}
+      <path d="M 40 65 C 45 80, 65 80, 70 65" stroke="#2C3E50" strokeWidth="3" fill="#E57373" strokeLinecap="round" />
     </svg>
   );
 }
