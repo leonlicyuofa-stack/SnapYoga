3bf798eafc4b65045580b924c59d903e8e16ba8b
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-15 15:28:19 +0000

    please replace pastel pink of female avatar to this color

--- src/components/icons/FemaleAvatar.tsx
+++ src/components/icons/FemaleAvatar.tsx
@@ -5,10 +5,10 @@ export function FemaleAvatar(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <rect x="15" y="15" width="70" height="70" rx="15" fill="hsl(var(--splash-blob-1))"/>
+        <rect x="15" y="15" width="70" height="70" rx="15" fill="#DFB2B5"/>
         {/* Eyes */}
-        <circle cx="43" cy="50" r="3" fill="#1A202C" />
-        <circle cx="57" cy="50" r="3" fill="#1A202C" />
+        <circle cx="43" cy="50" r="4" fill="#5c3c3c" />
+        <circle cx="57" cy="50" r="4" fill="#5c3c3c" />
       </g>
     </svg>
   );
