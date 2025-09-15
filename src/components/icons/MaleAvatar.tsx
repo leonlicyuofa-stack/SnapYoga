3f54ce8acf21d1566c70a6c444a645cc8942000d
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-15 15:23:24 +0000

    turn the male avatar to this please

--- src/components/icons/MaleAvatar.tsx
+++ src/components/icons/MaleAvatar.tsx
@@ -5,15 +5,10 @@ export function MaleAvatar(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <path 
-          d="M 85,50 C 95,75 75,95 50,95 C 25,95 5,75 15,50 C 25,25 40,15 50,20 C 60,25 75,25 85,50 Z"
-          fill="#FFF3B0"
-        />
+        <rect x="15" y="15" width="70" height="70" rx="15" fill="#608398"/>
         {/* Eyes */}
-        <circle cx="45" cy="50" r="4" fill="#2C3E50" />
-        <circle cx="65" cy="50" r="4" fill="#2C3E50" />
-        {/* Smile */}
-        <path d="M 48 60 C 52 68, 68 68, 72 60" stroke="#2C3E50" strokeWidth="3" fill="none" strokeLinecap="round" />
+        <circle cx="42" cy="50" r="5" fill="#2C3E50" />
+        <circle cx="58" cy="50" r="5" fill="#2C3E50" />
       </g>
     </svg>
   );
