77646fe455f47e63c17ce37ba235b3ade50ae727
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-16 15:13:09 +0000

    push male and female avatars 2x down

--- src/app/onboarding/gender-profile/page.tsx
+++ src/app/onboarding/gender-profile/page.tsx
@@ -92,7 +92,7 @@ export default function GenderProfilePage() {
         <div className="relative z-10 w-full max-w-sm flex flex-col items-center">
             <OnboardingHeader />
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 w-full -mt-16">
-              <div className="flex justify-around items-center pt-8">
+              <div className="flex justify-around items-center pt-16">
                   <div 
                     className={cn(
                         "cursor-pointer p-4 rounded-2xl transition-all w-48 h-auto flex flex-col items-center justify-center space-y-2 bg-card/20",
