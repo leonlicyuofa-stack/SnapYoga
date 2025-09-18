553c863c52d9ca337698b7e6341eb35a679e924a
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-18 15:54:40 +0000

    please make the spinning countdown ring 2x bigger, keep smileyrockloader

--- src/components/layout/smiley-rock-loader.tsx
+++ src/components/layout/smiley-rock-loader.tsx
@@ -13,7 +13,7 @@ interface SmileyRockLoaderProps {
 export function SmileyRockLoader({ className, text }: SmileyRockLoaderProps) {
   return (
     <div className={cn("flex items-center justify-center gap-2", className)}>
-      <div className="relative h-16 w-16">
+      <div className="relative h-32 w-32 flex items-center justify-center">
         <div className="absolute inset-0 border-4 border-primary/20 rounded-full"></div>
         <div className="absolute inset-0 border-4 border-transparent border-t-primary rounded-full animate-spin"></div>
         <SmileyPebbleIcon className="h-16 w-16" />
