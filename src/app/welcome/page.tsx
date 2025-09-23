c712d191b426901fff02abe959d39113fd12c952
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-23 11:24:44 +0000

    change to "WITHIN" (_for element p_)

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -51,7 +51,7 @@ export default function WelcomePage() {
             <div className="text-center text-primary animate-fade-in-up font-serif">
                 <p className="text-4xl tracking-widest">FIND</p>
                 <h1 className="text-6xl font-bold tracking-tighter my-2 font-playfair">your peace</h1>
-                <p className="text-4xl tracking-widest mt-2">BEGINS</p>
+                <p className="text-4xl tracking-widest mt-2">WITHIN</p>
             </div>
             
             <div className="mt-12 w-full max-w-xs animate-fade-in-up" style={{ animationDelay: '400ms' }}>
