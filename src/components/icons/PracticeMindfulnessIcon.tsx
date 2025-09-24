2cde7e70a5bf25d8be7a1e437662c20e00a1197e
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-24 07:32:29 +0000

    change the icon of "practice mindfulness" to an illustration similar to

--- src/components/icons/PracticeMindfulnessIcon.tsx
+++ src/components/icons/PracticeMindfulnessIcon.tsx
@@ -1,19 +1,25 @@
+
 import type { SVGProps } from 'react';
 
 export function PracticeMindfulnessIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
-        {/* Person */}
+        {/* Person's body */}
         <path d="M35 90 C 25 70, 30 50, 50 50 C 70 50, 75 70, 65 90" fill="hsl(var(--secondary))" stroke="none" />
-        <path d="M50 45 a 10 10 0 1 0 0.001 0" fill="hsl(var(--secondary))" stroke="none"/>
-        <path d="M45 65 L 55 65" />
         
-        {/* Eyes */}
+        {/* Head */}
+        <path d="M50 45 a 10 10 0 1 0 0.001 0" fill="hsl(var(--secondary))" stroke="none"/>
+
+        {/* Arms and Hands */}
+        <path d="M45 65 L 38 75" />
+        <path d="M55 65 L 62 75" />
+        
+        {/* Eyes (closed) */}
         <path d="M46 43 q2 -2 4 0" />
         <path d="M54 43 q-2 -2 -4 0" />
 
-        {/* Legs */}
+        {/* Legs crossed */}
         <path d="M35 90 C 45 80, 55 80, 65 90" />
         <path d="M38 90 C 45 85, 55 85, 62 90" />
       </g>
