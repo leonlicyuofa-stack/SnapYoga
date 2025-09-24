dd2264d487535c56b835efa9d7bdee9745f27a63
Georgina Poon <georgina.pnyc@gmail.com>
2025-09-24 14:19:06 +0000

    there's no change in the icon of practice mindfulness

--- src/components/icons/PracticeMindfulnessIcon.tsx
+++ src/components/icons/PracticeMindfulnessIcon.tsx
@@ -5,6 +5,9 @@ export function PracticeMindfulnessIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
+        {/* Cloud */}
+        <path d="M20 90 C10 90, 5 80, 20 75 C30 70, 40 70, 50 75 C60 70, 70 70, 80 75 C95 80, 90 90, 80 90 Z" fill="hsl(var(--muted) / 0.7)" stroke="none" />
+
         {/* Person's body */}
         <path d="M35 70 C 25 50, 30 30, 50 30 C 70 30, 75 50, 65 70" fill="hsl(var(--secondary))" stroke="none" />
         
@@ -22,9 +25,6 @@ export function PracticeMindfulnessIcon(props: SVGProps<SVGSVGElement>) {
         {/* Legs crossed */}
         <path d="M35 70 C 45 60, 55 60, 65 70" />
         <path d="M38 70 C 45 65, 55 65, 62 70" />
-
-        {/* Cloud */}
-        <path d="M20 90 C10 90, 5 80, 20 75 C30 70, 40 70, 50 75 C60 70, 70 70, 80 75 C95 80, 90 90, 80 90 Z" fill="hsl(var(--muted) / 0.7)" stroke="none" />
       </g>
     </svg>
   );
