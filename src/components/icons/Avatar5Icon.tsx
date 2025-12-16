af4dcba2d473f89dda836a14ac8cf752f3ea406f
Georgina Poon <georgina.pnyc@gmail.com>
2025-12-16 10:02:44 +0000

    replicate this image to replace avatar 5 icon

--- src/components/icons/Avatar5Icon.tsx
+++ src/components/icons/Avatar5Icon.tsx
@@ -5,28 +5,24 @@ export function Avatar5Icon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <title>Avatar with wobbly eyes</title>
+        <title>Avatar with a wide toothy grin</title>
         {/* The head/background is transparent so it can be colored by its container */}
         <path d="M 20,60 C 10,40 25,25 50,25 C 75,25 90,40 80,60 C 90,80 70,95 50,95 C 30,95 10,80 20,60 Z" fill="transparent" />
 
         {/* Eyes */}
-        <g>
-          {/* Left Eye */}
-          <circle cx="38" cy="48" r="14" fill="#FFFFFF" stroke="#000000" strokeWidth="2.5" />
-          <circle cx="38" cy="48" r="6" fill="#000000" />
-          <path d="M 28,64 Q 38,68 48,64" stroke="#000000" strokeWidth="2.5" fill="none" strokeLinecap="round" />
-          <path d="M 26,70 Q 38,74 50,70" stroke="#000000" strokeWidth="2.5" fill="none" strokeLinecap="round" />
-
-          {/* Right Eye */}
-          <circle cx="72" cy="48" r="14" fill="#FFFFFF" stroke="#000000" strokeWidth="2.5" />
-          <circle cx="72" cy="48" r="6" fill="#000000" />
-          <path d="M 62,64 Q 72,68 82,64" stroke="#000000" strokeWidth="2.5" fill="none" strokeLinecap="round" />
-          <path d="M 60,70 Q 72,74 84,70" stroke="#000000" strokeWidth="2.5" fill="none" strokeLinecap="round" />
-        </g>
+        <circle cx="42" cy="50" r="4" fill="#000000" />
+        <circle cx="58" cy="50" r="4" fill="#000000" />
         
-        {/* Mouth */}
-        <path d="M 50,82 C 46,82 45,86 50,86 C 55,86 54,82 50,82 Z" stroke="#000000" strokeWidth="2.5" fill="none" strokeLinejoin="round" />
+        {/* Mouth with Teeth */}
+        <g stroke="#000000" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+            {/* Mouth outline and fill */}
+            <path d="M 30,65 C 35,85 65,85 70,65 Z" fill="#FFFFFF" />
 
+            {/* Teeth */}
+            <path d="M 40 65 L 40 78" />
+            <path d="M 50 65 L 50 80" />
+            <path d="M 60 65 L 60 78" />
+        </g>
       </g>
     </svg>
   );
