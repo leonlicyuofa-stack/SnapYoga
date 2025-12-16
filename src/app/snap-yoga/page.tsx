314c39382e780417718b181fc89b3eb82f2f81d4
Georgina Poon <georgina.pnyc@gmail.com>
2025-12-16 17:09:57 +0000

    replicate the background of /practice-calendar to /snap-yoga

--- src/app/snap-yoga/page.tsx
+++ src/app/snap-yoga/page.tsx
@@ -2,13 +2,17 @@
 "use client";
 
 import { AppShell } from '@/components/layout/app-shell';
+import { QuadrantBackground } from '@/components/layout/QuadrantBackground';
 import { SnapYogaPageClient } from '@/components/features/snap-yoga/snap-yoga-page-client';
 
 export default function SnapYogaAnalysisPage() {
   return (
     <AppShell>
-      <div className="container mx-auto px-4 py-8">
-        <SnapYogaPageClient />
+      <div className="relative min-h-[calc(100vh-8rem)]">
+        <QuadrantBackground />
+        <div className="container mx-auto px-4 py-8 relative z-10">
+          <SnapYogaPageClient />
+        </div>
       </div>
     </AppShell>
   );
