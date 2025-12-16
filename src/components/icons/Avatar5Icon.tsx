cb3dbeef5bab26ebdbf5e4f61f72a1133a2010b8
Georgina Poon <georgina.pnyc@gmail.com>
2025-12-16 09:57:37 +0000

    replicate this image to replace avatar 5 icon

--- src/components/icons/Avatar5Icon.tsx
+++ src/components/icons/Avatar5Icon.tsx
@@ -4,21 +4,27 @@ export function Avatar5Icon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <title>Avatar of man with beard</title>
-        <circle cx="50" cy="50" r="40" fill="#D1FAE5" />
-        <path d="M50 80 C 65 95, 35 95, 50 80 L 50 60 Z" fill="#A7F3D0"/>
-        <path d="M35 55 C 30 45, 70 45, 65 55 Q 50 50 35 55 Z" fill="#1F2937"/>
-        <path d="M50 90 C 40 100, 60 100, 50 90 V 70 H 50 Z" fill="#111827"/>
-        <circle cx="50" cy="60" r="22" fill="#854d0e"/>
-        {/* Beard */}
-        <path d="M 38,70 C 35,80 65,80 62,70 Q 50,76 38,70 Z" fill="#1F2937" />
+        <title>Avatar with a big smile</title>
+        {/* The head/background is transparent so it can be colored by its container */}
+        <path d="M 20,60 C 10,40 25,25 50,25 C 75,25 90,40 80,60 C 90,80 70,95 50,95 C 30,95 10,80 20,60 Z" fill="transparent" />
+
         {/* Eyes */}
-        <g stroke="white" strokeWidth="2.5" fill="none">
-            <path d="M40 62 Q 44 60 48 62" strokeLinecap="round" />
-            <path d="M52 62 Q 56 60 60 62" strokeLinecap="round" />
-        </g>
-        {/* Mouth */}
-        <path d="M47,75 Q 50,80 53,75" stroke="#FFFFFF" strokeWidth="2" fill="none" strokeLinecap="round" />
+        <circle cx="42" cy="48" r="7" fill="#000000" />
+        <circle cx="68" cy="48" r="7" fill="#000000" />
+        
+        {/* Big Smile */}
+        <path 
+          d="M 28,62 C 25,75 75,75 72,62 C 70,55 30,55 28,62 Z" 
+          fill="#FFFFFF" 
+          stroke="#000000" 
+          strokeWidth="2.5" 
+          strokeLinejoin="round" 
+        />
+        
+        {/* Smile lines */}
+        <path d="M 32,62 C 35,65 35,65 32,68" stroke="#000000" fill="none" strokeWidth="2.5" strokeLinecap="round"/>
+        <path d="M 68,62 C 65,65 65,65 68,68" stroke="#000000" fill="none" strokeWidth="2.5" strokeLinecap="round"/>
+
       </g>
     </svg>
   );
