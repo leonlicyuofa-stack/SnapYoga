97588e150f87ae736856c086f812d84ba02df6c8
Georgina Poon <georgina.pnyc@gmail.com>
2025-12-16 09:48:54 +0000

    replicate this image to replace avatar 2 icon

--- src/components/icons/Avatar2Icon.tsx
+++ src/components/icons/Avatar2Icon.tsx
@@ -4,20 +4,20 @@ export function Avatar2Icon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <title>Avatar of person with blonde hair</title>
-        <circle cx="50" cy="50" r="40" fill="#FECDD3" />
-        <path d="M50 80 C 65 95, 35 95, 50 80 L 50 60 Z" fill="#FBCFE8"/>
-        {/* Hair */}
-        <path d="M30 60 C 20 40, 80 40, 70 60 L 65 35 L 35 35 Z" fill="#FDE047"/>
-        <path d="M50 90 C 40 100, 60 100, 50 90 V 70 H 50 Z" fill="#3D2C28"/>
-        <circle cx="50" cy="55" r="22" fill="#FEEBCB"/>
+        <title>Avatar of a cute pig</title>
+        {/* The head/background is transparent so it can be colored by its container */}
+        <path d="M 20,60 C 10,40 25,25 50,25 C 75,25 90,40 80,60 C 90,80 70,95 50,95 C 30,95 10,80 20,60 Z" fill="transparent" />
+
         {/* Eyes */}
-        <g stroke="#3D2C28" strokeWidth="2.5" fill="none">
-            <path d="M38 58 Q 42 56 46 58" strokeLinecap="round" />
-            <path d="M54 58 Q 58 56 62 58" strokeLinecap="round" />
-        </g>
-        {/* Mouth */}
-        <path d="M45,68 Q 50,73 55,68" stroke="#3D2C28" strokeWidth="2" fill="none" strokeLinecap="round" />
+        <circle cx="38" cy="48" r="8" fill="#000000" />
+        <circle cx="62" cy="48" r="8" fill="#000000" />
+
+        {/* Snout */}
+        <ellipse cx="50" cy="65" rx="22" ry="15" fill="#F48FB1" stroke="#E91E63" strokeWidth="2" />
+        
+        {/* Nostrils */}
+        <ellipse cx="44" cy="65" rx="4" ry="7" fill="#C2185B" />
+        <ellipse cx="56" cy="65" rx="4" ry="7" fill="#C2185B" />
       </g>
     </svg>
   );
