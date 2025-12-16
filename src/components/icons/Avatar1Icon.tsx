14ba34ede0d37c2d459ae8652f5e1423603147b9
Georgina Poon <georgina.pnyc@gmail.com>
2025-12-16 09:47:15 +0000

    replicate this image to replace avatar 1 icon

--- src/components/icons/Avatar1Icon.tsx
+++ src/components/icons/Avatar1Icon.tsx
@@ -4,26 +4,11 @@ export function Avatar1Icon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g>
-        <title>Avatar of man with glasses</title>
-        <circle cx="50" cy="50" r="40" fill="#F3E8FF" />
-        <path d="M50 80 C 65 95, 35 95, 50 80 L 50 60 Z" fill="#E9D5FF"/>
-        <path d="M35 55 C 30 40, 70 40, 65 55 L 60 70 L 40 70 Z" fill="#5A3E36"/>
-        <path d="M50 90 C 40 100, 60 100, 50 90 V 70 H 50 Z" fill="#3D2C28"/>
-        <circle cx="50" cy="55" r="22" fill="#FEEBCB"/>
-        <path d="M50 77 C 40 85, 60 85, 50 77 V 65 C 45 62, 55 62, 50 65 Z" fill="#C46D5E"/>
-        {/* Beard */}
-        <path d="M 38,62 C 35,72 65,72 62,62 Q 50,68 38,62 Z" fill="#5A3E36" />
-        {/* Glasses */}
-        <g stroke="#3D2C28" strokeWidth="2.5" fill="none">
-            <circle cx="42" cy="58" r="6" />
-            <circle cx="58" cy="58" r="6" />
-            <path d="M48 58 H 52" />
-        </g>
-        {/* Eyes */}
-        <circle cx="42" cy="58" r="2" fill="#3D2C28" />
-        <circle cx="58" cy="58" r="2" fill="#3D2C28" />
-        {/* Mouth */}
-        <path d="M45,70 Q 50,75 55,70" stroke="#FFFFFF" strokeWidth="2" fill="none" strokeLinecap="round" />
+        <title>Avatar of a cute frog</title>
+        <path d="M 20,60 C 10,40 25,25 50,25 C 75,25 90,40 80,60 C 90,80 70,95 50,95 C 30,95 10,80 20,60 Z" fill="#b2c8a4" />
+        <circle cx="38" cy="48" r="8" fill="#5a5a5a" />
+        <circle cx="62" cy="48" r="8" fill="#5a5a5a" />
+        <path d="M 48 55 Q 50 52 52 55" stroke="#5a5a5a" strokeWidth="2.5" fill="none" strokeLinecap="round" />
       </g>
     </svg>
   );
